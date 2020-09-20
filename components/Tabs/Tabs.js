3cf2d8fbbedecb697f88b7683b8b6a49002e2a66
unknown <unknown@example.com>
2020-09-20 19:39:21 +0800

    Initial Commit

---
+++ components/Tabs/Tabs.js
@@ -0,0 +1,28 @@
+// components/Tabs/Tabs.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+  },
+options:{
+  multipleSlots: true 
+
+},
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    test(){
+    console.log('aa')
+
+    }
+  }
+})
