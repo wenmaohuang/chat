d05699910d13688edd445c883fc2716b3e5d5f8b
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-06 00:55:54 +0800

    1

--- server/home.js
+++ server/home.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = 3005;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
