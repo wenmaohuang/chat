caafa1944d05ad567bb12bee58ff60ce0e2da140
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-14 22:00:40 +0800

    '1'

--- server_py/main.py
+++ server_py/main.py
@@ -1,11 +1,12 @@
-# -*- coding: utf-8 -*-
+:# -*- coding: utf-8 -*-
 # filename: main.py
-from handle import Handle
+"""from handle import Handle
 
 import web
 
 urls = (
-    '/wechat/getConfig', 'Handle',
+    '/wechat/g:q
+    :etConfig', 'Handle',
 )
 
 class Handle(object):
@@ -15,3 +16,41 @@ class Handle(object):
 if __name__ == '__main__':
     app = web.application(urls, globals())
     app.run()
+
+    """
+
+import falcon
+from falcon import uri
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy import parse_message
+from wechatpy .replies import TextReply,ImageReply
+
+class Connect(object):
+    def on_get(self,req, resp):
+        query_string = req.query_string
+        query_list = query_string.split('&')
+        b = {}
+        for i in query_list:
+            b[i.split('=')[0]] = i.split('=')[1]
+        try:
+            check_signature(token='这里填写配置界面你输入的token', signature=b[ 'signature'],timestamp=b['timestamp'], nonce=b['nonce'])
+            resp.body = (b['echostr'])
+        except InvalidsignatureException:
+            pass
+        resp.status = falcon.HTTP_200
+    def on_post(self,req,resp):
+        xml = req.stream.read()
+        msg= parse_message (xml)
+        if msg.type == 'text':
+            reply = TextReply(content=msg.content, message=msg)
+            xml = reply.render()
+            resp.body = (xml)
+            resp.status = falcon.HTTP_200
+app = falcon.API()
+connect = Connect()
+app.add_route('/wechat/getConfig', connect)
+
+
+
+
