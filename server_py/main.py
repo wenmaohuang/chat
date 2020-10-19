93b8aa6f373f33ab8029b1aebd2a98dae1f24e76
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-20 00:11:41 +0800

    '1'

--- server_py/main.py
+++ server_py/main.py
@@ -62,14 +62,15 @@ app.add_route('/wechat/getConfig', connect)
 WECHAT_TOKEN = "1234"
 from flask import Flask,request,abort
 import hashlib
+import xmltodict
+import time
 app = Flask(__name__)
-@app.route("/wechat8000")
+@app.route("/chat",methods = ["GET","POST"])
 def wechat():
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
     nonce = request.args.get("nonce")
-    echostr = request.args.get("echostr")
-    if not all ([signature,timestamp,nonce,echostr]):
+    if not all ([signature,timestamp,nonce]):
         abort(400)
     li = [WECHAT_TOKEN,timestamp,nonce]
     li.sort()
@@ -78,7 +79,44 @@ def wechat():
     if signature != sign:
         abort(403)
     else:
-        return echostr
+        if request.method = "GET":
+            echostr = request.args.get("echostr")
+            if not echostr:
+                abort(400)
+            return echostr
+        elif request.method = "POST":
+            xml_str = request.data
+            if not xml_str:
+                abort(400)
+            xml_dict = xmltodict.pasre(xml_str)
+            xml_dict = xml_dict.get("xml")
+            msg_type = xml_dict.get("MsgType")
+            if msg_type == "text":
+                resp_dict = {
+                "xml":{
+                "ToUserName":xml_dict.get("FromUserName"),
+                "FromUserName":xml_dict.get("ToUserName"),
+                "CreateTime":int(time.time()),
+                 "MsgType":"text",
+                 "Content":xml_dict.get("Content")
+
+                    }
+                }
+            else:
+                resp_dict = {
+                                "xml":{
+                                "ToUserName":xml_dict.get("FromUserName"),
+                                "FromUserName":xml_dict.get("ToUserName"),
+                                "CreateTime":int(time.time()),
+                                 "MsgType":"text",
+                                 "Content":"i love you"
+
+                                    }
+                                }
+
+            resp_xml_str = xmltodict.unparse(resp_dict)
+            return resp_xml_str
+
 
 
 if __name__ == '__main__':
