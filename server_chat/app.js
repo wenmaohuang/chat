638dc46985b20239602b64cce617c3b07b44c7fe
风移影动 <oncwnuFE28RNcGrpLh3eWbhn5qM4@git.weixin.qq.com>
2020-10-19 13:34:19 +0800

    '1'

--- server_chat/app.js
+++ server_chat/app.js
@@ -5,7 +5,7 @@ const auth = require('./weChat/auth')
 const app = express()
 app.use(express.static(path.join(__dirname, './public')))
 
-app.use(auth())
+app.use('/chat',auth())
 
 
 app.listen(3005,()=> console.log('服务器启动成功'))
