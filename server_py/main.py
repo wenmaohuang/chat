0ed5ce0351b81f4a3b6a3409b93b225ace7d95c0
root <wenmaohuang@hotmail.com>
2020-10-20 17:20:30 +0800

    2

--- server_py/main.py
+++ server_py/main.py
@@ -1,62 +1,6 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 
-"""
-
-from handle import Handle
-
-import web
-
-urls = (
-    '/', 'Handle',
-)
-
-class Handle(object):
-    def GET(self):
-        return "hello, this is handle view"
-
-if __name__ == '__main__':
-    app = web.application(urls, globals())
-    app.run()
-"""
-
-
-"""
-print('add .')
-import falcon
-from falcon import uri
-from wechatpy.utils import check_signature
-from wechatpy.exceptions import InvalidSignatureException
-from wechatpy import parse_message
-from wechatpy .replies import TextReply,ImageReply
-
-class Connect(object):
-    def on_get(self,req, resp):
-        query_string = req.query_string
-        query_list = query_string.split('&')
-        b = {}
-        for i in query_list:
-            b[i.split('=')[0]] = i.split('=')[1]
-        try:
-            check_signature(token='这里填写配置界面你输入的token', signature=b[ 'signature'],timestamp=b['timestamp'], nonce=b['nonce'])
-            resp.body = (b['echostr'])
-        except InvalidsignatureException:
-            pass
-        resp.status = falcon.HTTP_200
-    def on_post(self,req,resp):
-        xml = req.stream.read()
-        msg= parse_message (xml)
-        if msg.type == 'text':
-            reply = TextReply(content=msg.content, message=msg)
-            xml = reply.render()
-            resp.body = (xml)
-            resp.status = falcon.HTTP_200
-app = falcon.API()
-connect = Connect()
-app.add_route('/wechat/getConfig', connect)
-"""
-
-
 
 from gevent import pywsgi
 WECHAT_TOKEN = "1234"
@@ -70,7 +14,7 @@ app = Flask(__name__,
 
 print(app.instance_path,'bb')
 
-@app.route("/chat",methods = ["GET","POST"])
+@app.route("/chat/",methods = ["GET","POST"])
 def wechat():
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
@@ -81,7 +25,7 @@ def wechat():
     li = [WECHAT_TOKEN,timestamp,nonce]
     li.sort()
     tem_str = "".join(li)
-    sign = hashlib.sha1(tem_str).hexdigest()
+    sign = hashlib.sha1(tem_str.encode("utf-8")).hexdigest()
     if signature != sign:
         abort(403)
     else:
@@ -95,7 +39,7 @@ def wechat():
             print(xml_str)
             if not xml_str:
                 abort(400)
-            xml_dict = xmltodict.pasre(xml_str)
+            xml_dict = xmltodict.parse(xml_str)
             xml_dict = xml_dict.get("xml")
             msg_type = xml_dict.get("MsgType")
             print(msg_type)
@@ -129,49 +73,3 @@ if __name__ == '__main__':
     #server.serveOA_forever()
 
 
-"""
-# -*- coding:utf-8 -*-    #中文编码
-import sys
-#reload(sys) # 不加这部分处理中文还是会出问题
-import imp
-imp.reload(sys)
-
-#sys.setdefaultencoding('utf-8')
-import time
-from flask import Flask, request, make_response
-import hashlib
-import json
-import xml.etree.ElementTree as ET
-from dispatcher import *
-app = Flask(__name__)
-app.debug = True
-@app.route('/') # 默认网址
-def index():
- return 'Index Page'
-@app.route('/chat', methods=['GET', 'POST'])
-def wechat_auth(): # 处理微信请求的处理函数，get方法用于认证，post方法取得微信转发的数据
- if request.method == 'GET':
-     token = '1234'
-     data = request.args
-     signature = data.get('signature', '')
-     timestamp = data.get('timestamp', '')
-     nonce = data.get('nonce', '')
-     echostr = data.get('echostr', '')
-     s = [timestamp, nonce, token]
-     s.sort()
-     s = ''.join(s)
- if (hashlib.sha1(s).hexdigest() == signature):
-  return make_response(echostr)
- else:
-     rec = request.stream.read() # 接收消息
-     dispatcher = MsgDispatcher(rec)
-     data = dispatcher.dispatch()
- with open("./debug.log", "a") as file:
-  file.write(data)
-  file.close()
- response = make_response(data)
- response.content_type = 'application/xml'
- return response
-if __name__ == '__main__':
- app.run(host="0.0.0.0", port=3005)
- """
